bc3c757d548f91f63832cca7e538f93beb2722d6
Hao <zhsess@gmail.com>
2020-03-15 12:46:58 +0800

    v0.2 new Dispersion

---
+++ Dispersion/show_result.m
@@ -0,0 +1,6 @@
+load('result.mat')
+plot(data(:,1),data(:,2),'.')
+xlabel('Frequency(Hz)','FontSize',15)
+ylabel('Velocity(m/s)','FontSize',15)
+title('Dispertion Curve','FontSize',20)
+set(gca,'FontSize',12)
